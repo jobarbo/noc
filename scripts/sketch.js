eaa796814199a82534407aed611535d61dad9c5c
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-06-06 19:10:21 -0400

    exercise completed

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -18,13 +18,38 @@ function setup() {
 	c = createCanvas(DIM, DIM * 1.33);
 	pixelDensity(5);
 	background(255);
+
+	let card_num = 4;
+	let total_cards = 52;
+
+	let probability = (iterations, reshuffle = true) => {
+		if (reshuffle) {
+			// With replacement: same probability each time
+			return (card_num / total_cards) ** iterations;
+		} else {
+			// Without replacement: probability changes with each draw
+			let prob = 1;
+			let remaining_cards = total_cards;
+			let remaining_desired = card_num;
+
+			for (let i = 0; i < iterations; i++) {
+				prob *= remaining_desired / remaining_cards;
+				remaining_desired -= 1;
+				remaining_cards -= 1;
+			}
+			return prob;
+		}
+	};
+
+	let final_probability = probability(2, false);
+
+	// center the text
+	fill(0);
+	textSize(width / 15);
+	textAlign(CENTER, CENTER);
+	text(final_probability, width / 2, height / 2);
 }
 
 function draw() {
-	let hue = 1;
-	let size = random(1, 10);
-	console.log(size);
-	fill(hue, 255);
-	stroke(hue, 255);
-	circle(random(width), random(height), size);
+	// show text
 }
