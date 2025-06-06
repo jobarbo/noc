87a731f6e9a0a1d3e85ac357ce4ac05076dfd323
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-06-06 14:02:39 -0400

    first example

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,19 +1,36 @@
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
+// viewport
+let DEFAULT_SIZE = 948;
+let W = window.innerWidth;
+let H = window.innerHeight;
+let DIM;
+let MULTIPLIER;
+
+let randomCounts = [];
+
+let total = 50;
 
 function setup() {
-	let canvas = createCanvas(400, 400);
-	canvas.parent(select("main")); // Attach canvas to main frame container
-	pixelDensity(5);
-	background(255);
+	DIM = min(windowWidth, windowHeight);
+	MULTIPLIER = DIM / DEFAULT_SIZE;
+	c = createCanvas(DIM, DIM * 1.33);
+	pixelDensity(6);
+	for (let i = 0; i < total; i++) {
+		randomCounts[i] = 0; // initialize all counts to 0
+	}
 }
 
 function draw() {
-	let hue = 1;
-	let size = random(1, 10);
-	console.log(size);
-	fill(hue, 255);
-	stroke(hue, 255);
-	circle(random(width), random(height), size);
+	background(255, 255, 247);
+	let index = floor(random(randomCounts.length)); // pick a random index
+	randomCounts[index] += random(1, 50); // increment the count at that index
+
+	stroke(0);
+	fill(175);
+	let w = width / randomCounts.length;
+	for (let x = 0; x < randomCounts.length; x++) {
+		rect(x * w, height - randomCounts[x], w - 1, randomCounts[x]);
+	}
 }
