cba08097e9bc809d4692295ac2c52f2ec8c268ef
Jonathan Barbeau <barbeau.jonathan@gmail.com>
2025-08-13 09:39:32 -0400

    Exercise 0.3 completed

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -8,23 +8,51 @@ let H = window.innerHeight;
 let DIM;
 let MULTIPLIER;
 
-let randomCounts = [];
-
-let total = 50;
+let x, y;
+let w = 100;
+let speed = 13;
 
 function setup() {
 	DIM = min(windowWidth, windowHeight);
 	MULTIPLIER = DIM / DEFAULT_SIZE;
 	c = createCanvas(DIM, DIM * 1.33);
-	pixelDensity(5);
+	pixelDensity(2);
 	background(255);
+
+	x = width / 2;
+	y = height / 2;
 }
 
 function draw() {
-	let hue = 1;
-	let size = random(1, 10);
-	console.log(size);
-	fill(hue, 255);
-	stroke(hue, 255);
-	circle(random(width), random(height), size);
+	background(222, 255, 222);
+	fill(0);
+	ellipse(x, y, w, w);
+	move();
+}
+
+function move() {
+	let r = random(0, 1);
+	let r2 = random(0, 1);
+	if (r2 < 0.5) {
+		if (r < 0.25) {
+			x += speed;
+		} else if (r < 0.5) {
+			x -= speed;
+		} else if (r < 0.75) {
+			y += speed;
+		} else {
+			y -= speed;
+		}
+	} else {
+		if (x > mouseX) {
+			x -= speed;
+		} else if (x < mouseX) {
+			x += speed;
+		}
+		if (y > mouseY) {
+			y -= speed;
+		} else if (y < mouseY) {
+			y += speed;
+		}
+	}
 }
